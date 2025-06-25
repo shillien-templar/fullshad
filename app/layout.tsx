5ac63bdb71171e6e97e5f9fabce57e17de2da412
shillien-templar <shillien-templar@users.noreply.github.com>
2025-06-25 19:13:46 +0300

    Move sidebar and header into the layout and update the landing page component accordingly

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,10 @@ import type {Metadata} from "next";
 import {Inter} from "next/font/google";
 import "./globals.css";
 import {ThemeProvider} from "@/components/theme-provider";
+import {AppSidebar} from "@/components/app-sidebar";
+import {SidebarInset, SidebarProvider, SidebarTrigger} from "@/components/ui/sidebar";
+import {Separator} from "@/components/ui/separator";
+import {LayoutBreadcrumbs} from "@/components/layout/breadcrumbs";
 
 const inter = Inter({
     variable: "--font-inter",
@@ -29,7 +33,25 @@ export default function RootLayout({
             enableSystem
             disableTransitionOnChange
         >
-            {children}
+            <SidebarProvider>
+                <AppSidebar/>
+                <SidebarInset>
+                    <header
+                        className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+                        <div className="flex items-center gap-2 px-4">
+                            <SidebarTrigger className="-ml-1 cursor-pointer"/>
+                            <Separator
+                                orientation="vertical"
+                                className="mr-2 data-[orientation=vertical]:h-4"
+                            />
+                        </div>
+                    </header>
+
+                    <div>
+                        {children}
+                    </div>
+                </SidebarInset>
+            </SidebarProvider>
         </ThemeProvider>
         </body>
         </html>
